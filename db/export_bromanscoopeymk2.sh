68344d078a920bdb2c67e4efd765ec61fd09420a
EwanSC <ewan.coopey@hdr.mq.edu.au>
2022-05-31 16:09:15 +1000

    new export file and data folders

---
+++ db/export_bromanscoopeymk2.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+mkdir -p ../data
+
+sqlite3 BromansDBmk2.db -csv -header 'SELECT * FROM material ORDER BY MaterialID' > ../data/material.csv
+sqlite3 BromansDBmk2.db -csv -header 'SELECT * FROM inscribed_monument ORDER BY MonumentID' ../data/inscribed_monument.csv
+sqlite3 BromansDBmk2.db -csv -header 'SELECT * FROM small_find_artfact ORDER BY ArtefactID' ../data/small_find_artfact.csv
+sqlite3 BromansDBmk2.db -csv -header 'SELECT * FROM findspot ORDER BY FindSpotID' > ../data/findspot.csv
+sqlite3 BromansDBmk2.db -csv -header 'SELECT * FROM corpus ORDER BY CorpusName;' > ../data/corpus.csv
+sqlite3 BromansDBmk2.db -csv -header 'SELECT * FROM material_corpus ORDER BY MaterialCorpusID' > ../data/monument_corpus.csv
+sqlite3 BromansDBmk2.db -csv -header 'SELECT * FROM monument_serviceman ORDER BY MonumentServicemanID' > ../data/monument_serviceman.csv
+sqlite3 BromansDBmk2.db -csv -header 'SELECT * FROM legio_serviceman ORDER BY ServicemanID' > ../data/legio_serviceman.csv
+sqlite3 BromansDBmk2.db -csv -header 'SELECT * FROM military_status ORDER BY MilitaryStatusID' > ../data/military_status.csv
+sqlite3 BromansDBmk2.db -csv -header 'SELECT * FROM unit ORDER BY UnitID' > ../data/unit.csv
+
+
+mkdir -p ../analysis
+sqlite3 BromansDBmk2.db -csv -header 'SELECT * FROM all_material_with_location' > ../analysis/all_material_with_location.csv
